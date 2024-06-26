b1523b5d7520fde0d07d5835c61e32e04e21dd77
Haskili <connor.adams5461994@gmail.com>
2024-06-25 20:21:14 -0700

    Add introsort to sorting example, add commenting, etc.

--- Examples/sorting.cpp
+++ Examples/sorting.cpp
@@ -14,6 +14,7 @@ void insertionSort(int array[], int N);
 void bubbleSort(int array[], int N);
 void quickSort(int array[], int N);
 void mergeSort(int array[], int N);
+void introSort(int array[], int N);
 
 // Driver
 int main(int argc, char* argv[]) {
@@ -21,7 +22,7 @@ int main(int argc, char* argv[]) {
     // Define the input and perform
     // whatever preprocessing we need
     // (e.g. some randomization, etc.)
-    int input[5];
+    int input[10];
     int N = sizeof(input)/sizeof(*input);
     randomization(input, N);
 
@@ -35,10 +36,11 @@ int main(int argc, char* argv[]) {
     // Check the final product and print the results
     printf("%s\n", check(input, N)? "Failed":"Passed");
 
+    // Print the final output array for a visual check
     for (int i = 0; i < N; i++)
         printf("%i%s", input[i], i == (N-1)? "\n":", ");
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 /*
@@ -82,6 +84,19 @@ void randomization(int array[], int N) {
         array[i] = dist6(rng);
 }
 
+/*
+    Perform selection sort;
+
+    The point of selection sort is more or less
+    as follows,
+
+        - Loop through all indices 'i' of 'array'
+        - For each iteration find what belongs at 'i'
+
+    That's to say that it's an O(n^2) time complexity algorithm.
+    It's not pretty, but it serves as a good introduction into 
+    how sorting works.
+*/
 void selectionSort(int array[], int N) {
 
     // Early return conditions
@@ -89,7 +104,7 @@ void selectionSort(int array[], int N) {
         return;
 
     // For every element in 'array' [0..N]
-    for (int i = 0, temporary = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
 
         // Find the index of minimum value element
         // in the subarray array[i..N]
@@ -102,27 +117,51 @@ void selectionSort(int array[], int N) {
         // If we found a new minimum value,
         // swap the values at the 'minimum'
         // and the current position 'i'
-        if (minimum != i) {
-            temporary = array[i];
-            array[i] = array[minimum];
-            array[minimum] = temporary;
-        }
+        if (minimum != i)
+            array[i] ^= array[minimum], 
+            array[minimum] ^= array[i], 
+            array[i] ^= array[minimum];
     }
 }
 
+/*
+    TODO
+*/
 void insertionSort(int array[], int N) {
     // TODO
 }
 
+/*
+    Perform bubble sort;
+
+    The point of bubble sort is sort of
+    similar to selection sort from the
+    right angle. It works as follows,
+
+        - Continue iteration while 'flag'
+          isn't set to 'true'
+
+        - Iterate through 'i' indices [1..N] of 'array'
+        - For each iteration 'i' we may swap the 
+          the elements at 'i' & 'i - 1'
+
+        - After the 'i' iterations if we see
+          that there weren't any swaps via 'flag'
+          we can return to the caller
+
+    Visually looking at the algorithm in action, we
+    see that elements "bubble" to the right position,
+    thus the name. Similar to selection sort this has
+    a O(n^2) time complexity.
+*/
 void bubbleSort(int array[], int N) {
 
     // Early return conditions
     if (N == 0 || N == 1)
         return;
 
-    // Initialize a 'flag' variable and
-    // something to hold temporary values
-    int flag = 1, temporary = 0;
+    // Initialize a 'flag' variable
+    int flag = 1;
     
     // Continue iteration until something
     // inside our loop breaks us out
@@ -136,9 +175,9 @@ void bubbleSort(int array[], int N) {
             // If we need to perform a swap,
             // do so and toggle off the 'flag'
             if (array[i - 1] > array[i]) {
-                temporary = array[i - 1];
-                array[i -1] = array[i];
-                array[i] = temporary;
+                array[i] ^= array[i - 1], 
+                array[i - 1] ^= array[i], 
+                array[i] ^= array[i - 1];
                 flag = 0;
             }
         }
@@ -154,10 +193,23 @@ void bubbleSort(int array[], int N) {
     }
 }
 
+/*
+    TODO
+*/
 void quickSort(int array[], int N) {
     // TODO
 }
 
+/*
+    TODO
+*/
 void mergeSort(int array[], int N) {
+    // TODO
+}
+
+/*
+    TODO
+*/
+void introSort(int array[], int N) {
     // TODO
 }
